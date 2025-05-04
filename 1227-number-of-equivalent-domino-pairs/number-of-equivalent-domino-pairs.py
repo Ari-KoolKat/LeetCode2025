05910dc91465e6ed05e3e4d850aba53cb9a040f7
Ari-KoolKat <arianamilik@gmail.com>
2025-05-04 21:04:53 +0200

    Time: 23 ms (45.33%) | Memory: 19 MB (82.67%) - LeetSync

---
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numEquivDominoPairs(self, dominoes):
+        """
+        :type dominoes: List[List[int]]
+        :rtype: int
+        """
+        count = defaultdict(int)
+        res = 0
+
+        for a, b in dominoes:
+            key = tuple(sorted((a, b)))  
+            res += count[key]            
+            count[key] += 1              
+
+        return res
