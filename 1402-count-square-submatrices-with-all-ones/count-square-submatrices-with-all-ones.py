57c91f9c91b1c15763f559bcb94e25aed262c93c
Ari-KoolKat <arianamilik@gmail.com>
2025-08-20 12:52:21 +0200

    Time: 67 ms (81.82%) | Memory: 13.6 MB (64.55%) - LeetSync

---
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countSquares(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        t=0
+        r=len(matrix)
+        c=len(matrix[0])
+        for i in range(r):
+            for j in range(c):
+                if matrix[i][j]==1:
+                    if i>0 and j>0:
+                        matrix[i][j]=1+min(matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1])
+                    t+=matrix[i][j]
+        return t
