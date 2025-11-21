9370fa6f4e243cb6cd2395b8181efb2799f037ca
Ari-KoolKat <arianamilik@gmail.com>
2025-11-21 23:21:48 +0100

    Time: 131 ms (100.00%) | Memory: 12.9 MB (95.83%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -4,17 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        first_occ = {}
-        last_occ = {}
-        for i, char in enumerate(s):
-            if char not in first_occ:
-                first_occ[char] = i 
-            last_occ[char] = i
-        unique_palindromes = set()
-        for char in first_occ:
-            start = first_occ[char]
-            end = last_occ[char]
-            if end - start > 1:
-                for middle_char in set(s[start + 1: end]):
-                    unique_palindromes.add((char, middle_char, char))
-        return len(unique_palindromes)            
+        ans = 0
+        
+        # Loop through each lowercase letter
+        for ch in set(s):  # Only consider characters that exist in 's'
+            first = s.find(ch)  # Find the first occurrence of the character
+            last = s.rfind(ch)  # Find the last occurrence of the character
+            
+            if first != -1 and last != -1 and first < last:
+                # Use a set to find unique characters between first and last
+                ans += len(set(s[first + 1:last]))
+        
+        return ans
