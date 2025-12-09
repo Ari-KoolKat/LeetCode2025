32df63e1ce074932d9beb3b1a99c5160d104a56e
Ari-KoolKat <arianamilik@gmail.com>
2025-12-09 18:18:13 +0100

    Time: 1499 ms (9.09%) | Memory: 29.9 MB (22.73%) - LeetSync

---
+++ 3885-count-special-triplets/count-special-triplets.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def specialTriplets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        mod = 10 ** 9 + 7
+
+        ans, n = 0, len(nums)
+        left_count = Counter()
+        right_count = Counter(nums)
+        for i in range(n):
+            right_count[nums[i]] -= 1
+
+            target = nums[i] * 2
+            ans += (left_count[target] * right_count[target]) % mod
+
+            left_count[nums[i]] += 1
+
+        return ans % mod
